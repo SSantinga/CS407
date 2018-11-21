46e17331989d4520da7900972612311028ea8697
smsantinga <smsantinga@crimson.ua.edu>
2018-11-20 19:47:58 -0600

    Navbar edits - still working on functionality but it's a nav bar now at least.

--- myShift/src/app/navigation/navigation.component.ts
+++ myShift/src/app/navigation/navigation.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
+  imports: [CommonModule],
+  declarations: [NavigationComponent],
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
+
+
 export class NavigationComponent implements OnInit {
 
   constructor() { }
@@ -13,3 +18,5 @@ export class NavigationComponent implements OnInit {
   }
 
 }
+
+
